8275a28d6366bd01bd12a846e8519867e92c5d35
Artem Abzanov <jerrycauser@gmail.com>
2020-02-20 01:20:00 +0300

    update: refactor scraping data via websockets

--- sc2moneyline.js
+++ sc2moneyline.js
@@ -1,47 +1,86 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
-async function get_money_line() {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://gg22.bet/en/starcraft2', { waitUntil: 'networkidle2' });
+function getMatches (page, reliably) {
+  return new Promise(async resolve => {
+    const result = {}
 
-    const starcraft2matches = await page.evaluate(() => {
-        let object = []
-        let j = 0
-        let names = document.querySelectorAll('.__app-LogoTitle-name.LogoTitle__name___2LTlu')
-        let odds = document.querySelectorAll('.odd__ellipsis___3b4Yk')
-        let date_time = document.querySelectorAll('.dateTime__date___147AU')
+    const handleWebSocketFrameReceived = params => {
+      try {
+        const data = JSON.parse(params.response.payloadData)
 
-        function get_day(text) {
-            if (text.indexOf('TODAY') !== -1) {
-                let day = new Date()
-                day = day.toLocaleDateString('en-EN', { month: 'short', day: 'numeric' })
-                day = day.toUpperCase()
-                return text.replace('TODAY', day)
-            } else {
-                return text
-            }
+        if (data && data.payload && data.payload.data && data.payload.data.matches) {
+
+          const { matches } = data.payload.data
+
+          for (const match of matches.filter(Boolean)) {
+            try {
+              const winnerMarket = match.markets.find(market => /^winner$/i.test(market.name))
+
+              if (winnerMarket) {
+                const { id, fixture } = match
+                const { competitors, score, status, startTime } = fixture
+
+                if (result[id] !== undefined) {
+                  resolve(result)
+                  break
+                }
+
+                const { name: home } = competitors.find(cmp => /home/i.test(cmp.homeAway))
+                const { name: away } = competitors.find(cmp => /away/i.test(cmp.homeAway))
+                const { value: homeOdd } = winnerMarket.odds.find(odd => odd.name === home)
+                const { value: awayOdd } = winnerMarket.odds.find(odd => odd.name === away)
+
+                result[id] = {
+                  id,
+                  originalId: id.length > 36 ? id.slice(-36) : id,
+                  score,
+                  status,
+                  startTime: +new Date(startTime),
+                  home,
+                  away,
+                  homeOdd,
+                  awayOdd
+                }
+              }
+            } catch (e) {}
+          }
+
+          if (!reliably) {
+            resolve(result)
+          }
         }
+      } catch (e) {}
+    }
 
-        for (let i = 0; i < names.length; i++) {
-            if (i == 0)
-                object[j] = []
-            if (i % 2 == 0 && i != 0) {
-                j++
-                object[j] = []
-                object[j].push({ time: get_day(date_time[j].innerText), name: names[i].innerText, odds: odds[i].innerText })
-            } else {
-                object[j].push({ time: get_day(date_time[j].innerText), name: names[i].innerText, odds: odds[i].innerText })
-            }
-        }
+    const f12 = await page.target().createCDPSession()
+    await f12.send('Network.enable')
+    await f12.send('Page.enable')
 
-        return object
-    })
+    f12.on('Network.webSocketFrameReceived', handleWebSocketFrameReceived)
+  })
+}
 
-    await browser.close();
-    return starcraft2matches
+/**
+ * @param {boolean} [reliably=false] if true will parse websocket until found duplicate.
+ *                                   otherwise will resolve after first success parsing
+ * @returns {Promise<object>}
+ */
+async function get_money_line (reliably = false) {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await page.goto('https://gg22.bet/en/starcraft2', { waitUntil: 'domcontentloaded' })
+
+  const matches = await getMatches(page, reliably)
+
+  await page.close()
+  await browser.close()
+
+  return matches
 }
 
 module.exports = {
-    get_money_line : get_money_line
+  get_money_line
 }
+
+get_money_line().then(res => console.log(res))
